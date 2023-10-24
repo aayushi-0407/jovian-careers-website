48b2c02e7a3b67fc92320f843f0602f02647ede2
aayushi-0407 <117918926+aayushi-0407@users.noreply.github.com>
2023-10-24 16:15:50 +0000

    made flask server

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
+from flask import Flask, json, render_template, jsonify
 
-from flask import Flask
+app = Flask(__name__)
 
-
-app= Flask(__name__) ##creating app
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': '$10000K - Rs. $200000K'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': '$300000K'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$150,000'
+  }
+]
 
 @app.route("/")
-def hello_world():
-  return "Hello Welcome to out website"
+def hello_jovian():
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='Jovian')
 
-if __name__== "__main__":
-  app.run(host='0.0.0.0',debug=True)
+
+@app.route("/api/jobs")
+def jobs():
+  return jsonify(JOBS)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
---
+++ exor.jpg.v
@@ -0,0 +1,6 @@
+module exorgate(
+	input a_i,
+	input b_i,
+	input c_o);
+	assign c_o=a_i^b_i;
+	end module
